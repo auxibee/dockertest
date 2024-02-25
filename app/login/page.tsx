43bad84dfd00310970722c66a937a0c683978d43
Yaw Twumasi Acheampong <auxibee@gmail.com>
2024-02-24 20:38:25 -0500

    setting ci/cd

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,10 @@ export default function Page() {
   return (
     <div>
       <p>Hello this is login here....</p>
+      <form action="">
+        <input type="text" placeholder="username" />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
