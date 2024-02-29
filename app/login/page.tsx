e0cd59cc89d07d06bf499e439978e6a8a934eca7
Yaw Twumasi Acheampong <auxibee@gmail.com>
2024-02-28 21:49:38 -0500

    ci

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
       <p>Hello this is login here....</p>
       <form action="">
         <input type="text" placeholder="username" />
+        <input type="password" placeholder="******" />
         <button type="submit">Login</button>
       </form>
     </div>
